e42d9a0c21da41ad44136a5c758ac4393c741ac2
Josh Levy <joshaaronlevy@gmail.com>
2019-10-13 12:51:33 -0600

    "Simplifying"

--- bin/index.js
+++ bin/index.js
@@ -1,11 +1,22 @@
 #!/usr/bin/env node
 const program = require('commander');
-const gitquick = require('../lib/gitquick');
+// const gitquick = require('../lib/gitquick');
+const runner = require('../lib/runner.js');
+
+// program
+//   .description('Add, Commit, and Push')
+//   .action(function () {
+//     gitquick();
+//   });
+
+// program.parse(process.argv);
 
 program
+  .option('[message]')
   .description('Add, Commit, and Push')
-  .action(function () {
-    gitquick();
+  .action((message) => {
+    console.log(message)
+    runner(message);
   });
 
 program.parse(process.argv);
