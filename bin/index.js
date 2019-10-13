068c4eac78ac12cff4716cf77c9591db51ec0564
Josh Levy <joshaaronlevy@gmail.com>
2019-10-13 13:26:27 -0600

    "Updated ora messages"

--- bin/index.js
+++ bin/index.js
@@ -7,9 +7,9 @@ program
   .option('[message]')
   .description('Add, Commit, and Push')
   .action(async (message) => {
-    const spinner = ora('Committing and pushing...').start()
+    const spinner = ora('Nice code! Adding, committing and pushing...').start()
     await runner(message);
-    spinner.succeed('Nice jerb!');
+    spinner.succeed('Success!');
   });
 
 program.parse(process.argv);
