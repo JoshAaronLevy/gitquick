28da1cdea09913d731d3f1bd5cc2cd96f40c211f
Josh Levy <joshaaronlevy@gmail.com>
2019-10-13 13:05:14 -0600

    "Implementing Ora"

--- bin/index.js
+++ bin/index.js
@@ -8,7 +8,6 @@ program
   .description('Add, Commit, and Push')
   .action((message) => {
     ora('Nice code! Committing and pushing...').start()
-    // console.log('Nice code! Committing and pushing to git...')
     runner(message);
     ora().stop();
   });
