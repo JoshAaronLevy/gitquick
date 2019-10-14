6460313a515ee64ecaa1c7852fc97b4a9d70eda7
Josh Levy <joshaaronlevy@gmail.com>
2019-10-13 23:19:35 -0600

    "Pruning dependencies"

--- bin/index.js
+++ bin/index.js
@@ -7,7 +7,7 @@ program
   .option('[message]')
   .description('Add, Commit, and Push')
   .action(async (message) => {
-    const spinner = ora('Nice code! Adding, committing and pushing...').start()
+    const spinner = ora('Adding, committing and pushing [message]').start()
     await runner(message);
     spinner.succeed('Success!');
   });
