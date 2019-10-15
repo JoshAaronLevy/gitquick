ecf27e507cca15b9b625335cfec1fbec8c85b19e
Josh Levy <joshaaronlevy@gmail.com>
2019-10-15 15:48:24 -0600

    "Updated flow with proper success/error handling and updated to version 1.0.1"

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,11 @@
 #!/usr/bin/env node
 const program = require('commander');
 const runner = require('../lib/runner.js');
-const ora = require('ora');
 
 program
   .description(`Example: gitquick "I fixed a bug"`)
   .option('[message]')
-  .version('0.14.1', '-v, --version')
+  .version('1.0.1', '-v, --version')
   .action(async (message) => {
     await runner(message);
   });
