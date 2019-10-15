2749b507addc4b51cca46df77fde0509305c6d62
Josh Levy <joshaaronlevy@gmail.com>
2019-10-15 11:32:13 -0600

    "Testing logs"

--- bin/index.js
+++ bin/index.js
@@ -1,15 +1,12 @@
 #!/usr/bin/env node
 const program = require('commander');
 const runner = require('../lib/runner.js');
-// const ora = require('ora');
 
 program
   .option('[message]')
   .description('Add, Commit, and Push')
   .action(async (message) => {
-    // const spinner = ora(`Nice code! Adding, committing and pushing...`).start()
     await runner(message);
-    // spinner.succeed(`"${message}" successfully committed and pushed!`);
   });
 
 program.parse(process.argv);
