67d20cbe6b61710f5b068b2095f7e95d6eb26e96
Josh Levy <joshaaronlevy@gmail.com>
2019-10-15 12:58:13 -0600

    "Added help option"

--- bin/index.js
+++ bin/index.js
@@ -1,20 +1,14 @@
 #!/usr/bin/env node
 const program = require('commander');
 const runner = require('../lib/runner.js');
+const ora = require('ora');
 
 program
-  .version('0.12.0', '-v, --version')
-  .option('[message]', `Example: gitquick "I added some features"`)
-  .option('-h, --help')
+  .option('[message]')
+  .version('0.14.0', '-v, --version')
   .description('Add, Commit, and Push on the Fly')
   .action(async (message) => {
     await runner(message);
   });
-
-// program.on('-h, --help', function(){
-//   console.log('');
-//   console.log('Example:');
-//   console.log(`gitquick "I added some features"`);
-// });
 
 program.parse(process.argv);
