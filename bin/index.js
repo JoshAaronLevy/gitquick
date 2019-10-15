17ac5ceff359aab528d1c78a1a46d0679a7fc361
Josh Levy <joshaaronlevy@gmail.com>
2019-10-15 11:10:24 -0600

    "Testing logs"

--- bin/index.js
+++ bin/index.js
@@ -3,12 +3,23 @@ const program = require('commander');
 const runner = require('../lib/runner.js');
 const ora = require('ora');
 
+// program
+//   .option('[message]')
+//   .description('Add, Commit, and Push')
+//   .action(async (message) => {
+//     // const spinner = ora(`Nice code! Adding, committing and pushing...`).start()
+//     await runner(message);
+//     // spinner.succeed(`"${message}" successfully committed and pushed!`);
+//   });
+
+// program.parse(process.argv);
+
 program
   .option('[message]')
   .description('Add, Commit, and Push')
-  .action(async (message) => {
+  .action(function(message) {
     // const spinner = ora(`Nice code! Adding, committing and pushing...`).start()
-    await runner(message);
+    runner(message);
     // spinner.succeed(`"${message}" successfully committed and pushed!`);
   });
 
