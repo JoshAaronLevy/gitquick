a9552915163ca7b9a6fe3dd24d79ef9185bef479
Josh Levy <joshaaronlevy@gmail.com>
2019-10-15 13:02:19 -0600

    "Added version option and updated version to 0.14.1 for release"

--- bin/index.js
+++ bin/index.js
@@ -4,9 +4,9 @@ const runner = require('../lib/runner.js');
 const ora = require('ora');
 
 program
+  .description(`Example: gitquick "I fixed a bug"`)
   .option('[message]')
-  .version('0.14.0', '-v, --version')
-  .description('Add, Commit, and Push on the Fly')
+  .version('0.14.1', '-v, --version')
   .action(async (message) => {
     await runner(message);
   });
