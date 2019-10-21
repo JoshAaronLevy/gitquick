eda932a479e69a8865f09164de8cc54bc5ca8527
Josh Levy <joshaaronlevy@gmail.com>
2019-10-21 17:31:35 -0600

    "Finished updating improved flow"

--- bin/index.js
+++ bin/index.js
@@ -6,7 +6,7 @@ program
   .description(`Example: gitquick "I fixed a bug"`)
   .option('[message]')
   .option('-c, --commit')
-  .version('2.1.1', '-v, --version')
+  .version('2.5.2', '-v, --version')
   .action(async (message, command) => {
     let commit = command.commit;
     if (!commit) {
