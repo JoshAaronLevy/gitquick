59de4e739d066fd66cb1638c5c95c701317c2a87
JoshAaronLevy <joshaaronlevy@gmail.com>
2023-03-04 16:32:38 -0700

    Updated to version 4.0.2

--- bin/index.js
+++ bin/index.js
@@ -6,7 +6,7 @@ program
 	.description("Example: gitquick \"I fixed a bug\"")
 	.option("[message]")
 	.option("-c, --commit")
-	.version("2.11.16", "-v, --version")
+	.version("4.0.2", "-v, --version")
 	.action(async (message, command) => {
 		let commit = command.commit;
 		if (!commit) {
