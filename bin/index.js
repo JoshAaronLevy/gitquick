deec2653b61e2515f3106ac140c104c8df7a4431
JoshAaronLevy <joshaaronlevy@gmail.com>
2023-05-26 07:02:05 -0600

    Added

--- bin/index.js
+++ bin/index.js
@@ -1,20 +1,23 @@
 #!/usr/bin/env node
-const program = require("commander");
-const runner = require("../lib/runner.js");
-const { inputCommitMessage } = require("../lib/prompt.js");
+const program = require('commander');
+const runner = require('../lib/runner.js');
+const { inputCommitMessage } = require('../lib/prompt.js');
 
 program
-	.description("Example: gitquick \"I fixed a bug\"")
-	.argument("[message]")
-	.version("1.2.2", "-v, --version")
+	.description('Example: gitquick "I fixed a bug"')
+	.argument('[message]')
+	.version('4.4.3', '-v, --version')
 	.action(async (message) => {
 		try {
-			console.log("program: ", program);
 			if (!message) message = await inputCommitMessage();
 			if (message) {
+				if (message[0] === '"' && (program.args && program.args.length > 1)) {
+					message = program.args.join(' ');
+				}
+				console.log('program: ', program);
 				return await runner(message);
 			} else {
-				console.log("Unable to initiate commit process. Please try again.");
+				console.log('Unable to initiate commit process. Please try again.');
 			}
 		} catch (error) {
 			console.error(error);
