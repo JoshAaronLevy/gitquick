a7d894d493846e891c158b94c075dfbefe550d37
JoshAaronLevy <joshaaronlevy@gmail.com>
2023-05-26 05:46:13 -0600

    Hello

--- bin/index.js
+++ bin/index.js
@@ -1,35 +1,15 @@
 #!/usr/bin/env node
-const commander = require("commander");
-const program = new commander.Command();
+const program = require("commander");
 const runner = require("../lib/runner.js");
-const prompt = require("../lib/prompt.js");
-
-// program
-// 	.name("gitquick")
-// 	.description("CLI to add, commit and push changes to remote repository in one step")
-// 	.version("4.4.2");
-
-console.log("process.argv: ", process.argv);
+const { inputCommitMessage } = require("../lib/prompt.js");
 
 program
 	.description("Example: gitquick \"I fixed a bug\"")
 	.option("[message]")
-	.option("-c, --commit", "Commit changes only without pushing to remote repository", false)
-	.version("4.4.2")
-	.action(async (command, options) => {
-		let message;
-		// console.log("command: ", command);
-		console.log("options: ", options);
-		// console.log("commitFlag: ", commitFlag);
-		let commit = false;
-		// (!options || (options && !options.commit)) ? commit = false : commit = true;
-		// console.log("commit: ", commit);
-		command ? message = command.trim() : message = await prompt.commitMessageInput();
-		if (!message) return;
-		await runner(message, commit);
+	.version("1.2.2", "-v, --version")
+	.action(async (message) => {
+		console.log("message: ", message);
+		await runner(message);
 	});
 
 program.parse(process.argv);
-
-const options = program.opts();
-console.log("options: ", options);
