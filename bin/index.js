5c5173fb498bfe891cd4b716bd00936cdc96bb24
JoshAaronLevy <joshaaronlevy@gmail.com>
2023-05-26 06:42:44 -0600

    Refactored with commit message conditional prompt

--- bin/index.js
+++ bin/index.js
@@ -6,20 +6,18 @@ const { inputCommitMessage } = require("../lib/prompt.js");
 program
 	.description("Example: gitquick \"I fixed a bug\"")
 	.argument("[message]")
-	.option("-c, --commit-only", "Commit changes only. Skip pushing to remote repository")
 	.version("1.2.2", "-v, --version")
 	.action(async (message) => {
-		// console.log("program: ", program);
-		console.log("message: ", message);
-		console.log("options: ", program.opts());
-		if (program.opts().commitOnly) {
-			console.log("Commit only flag is set");
-		}
-		if (!message) message = await inputCommitMessage();
-		if (message) {
-			return await runner(message);
-		} else {
-			console.log("No message provided");
+		try {
+			console.log("program: ", program);
+			if (!message) message = await inputCommitMessage();
+			if (message) {
+				return await runner(message);
+			} else {
+				console.log("Unable to initiate commit process. Please try again.");
+			}
+		} catch (error) {
+			console.error(error);
 		}
 	});
 
