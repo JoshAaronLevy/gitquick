064ee5962376f4664e1972ba5fac300b4c5c3230
JoshAaronLevy <joshaaronlevy@gmail.com>
2023-05-26 01:56:09 -0600

    Hello

--- bin/index.js
+++ bin/index.js
@@ -6,10 +6,11 @@ const prompt = require("../lib/prompt.js");
 program
 	.description("Example: gitquick \"I fixed a bug\"")
 	.option("[message]")
-	.option("-c, --commit")
+	.option("-c, --commit", "Commit changes only without pushing to remote repository", (commitFlag) => commitFlag)
 	.version("4.3.7", "-v, --version")
-	.action(async (message, command) => {
+	.action(async (message, command, commitFlag) => {
 		console.log("command: ", command);
+		console.log("commitFlag: ", commitFlag);
 		let commit;
 		(!command || (command && !command.commit)) ? commit = false : commit = true;
 		console.log("commit: ", commit);
