b0f43f28d8bb671da805e2a31cc717f96c059ec6
Josh Levy <JoshAaronLevy@users.noreply.github.com>
2023-05-26 07:14:28 -0600

    4.5.3: Added prompt and bugfix conditions (#4)
    * Hello
    
    * Hello
    
    * Hello
    
    * Hello
    
    * Hello
    
    * Hello
    
    * Hello
    
    * Hello
    
    * Hello
    
    * Hello
    
    * Hello
    
    * Hello
    
    * Hello
    
    * Hello
    
    * Hello
    
    * Hello
    
    * Hello
    
    * Hello
    
    * Hello
    
    * Hello
    
    * Refactored with commit message conditional prompt
    
    * Cleaning
    
    * Added
    
    * Added
    
    * Added
    
    * Added
    
    * Added console log
    
    * Removed console log
    
    * Updated to v4.5.3

--- bin/index.js
+++ bin/index.js
@@ -1,20 +1,27 @@
 #!/usr/bin/env node
-const program = require("commander");
-const runner = require("../lib/runner.js");
+const program = require('commander');
+const runner = require('../lib/runner.js');
+const { inputCommitMessage } = require('../lib/prompt.js');
 
 program
-	.description("Example: gitquick \"I fixed a bug\"")
-	.option("[message]")
-	.option("-c, --commit")
-	.version("4.3.7", "-v, --version")
-	.action(async (message, command) => {
-		let commit = command.commit;
-		if (!commit) {
-			commit = false;
-		} else {
-			commit = true;
+	.description('Example: gitquick "I fixed a bug"')
+	.argument('[message]')
+	.version('4.5.3', '-v, --version')
+	.action(async (message) => {
+		const processArgs = process.argv.slice(2);
+		if (processArgs.length > 1) {
+			message = processArgs.slice(0, processArgs.length).join(' ');
 		}
-		await runner(message, commit);
+		try {
+			if (!message) message = await inputCommitMessage();
+			if (message) {
+				return await runner(message);
+			} else {
+				console.log('Unable to initiate commit process. Please try again.');
+			}
+		} catch (error) {
+			console.error(error);
+		}
 	});
 
 program.parse(process.argv);
