b1e1919eda810e770f0e5fa82c0bd1edbc80d557
JoshAaronLevy <joshaaronlevy@gmail.com>
2023-05-26 06:12:50 -0600

    Hello

--- bin/index.js
+++ bin/index.js
@@ -5,14 +5,21 @@ const { inputCommitMessage } = require("../lib/prompt.js");
 
 program
 	.description("Example: gitquick \"I fixed a bug\"")
-	.option("[message]")
 	.option("-c, --commit-only", "Commit changes only. Skip pushing to remote repository")
 	.version("1.2.2", "-v, --version")
+	.arguments("[message]")
 	.action(async (message, command) => {
 		console.log("message: ", message);
 		console.log("command: ", command);
-		if (message?.args?.length === 0) message = await inputCommitMessage();
-		return await runner(message);
+		if (command.commitOnly) {
+			console.log("Commit only flag is set");
+		}
+		if (!message) message = await inputCommitMessage();
+		if (message) {
+			return await runner(message);
+		} else {
+			console.log("No message provided");
+		}
 	});
 
 program.parse(process.argv);
