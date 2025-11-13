2580385d695d423b4b191642722cca71f82e6bd7
Josh Levy <joshua.a.levy@xcelenergy.com>
2025-11-13 15:08:32 -0700

    Updated summary

--- src/lib/runner.ts
+++ src/lib/runner.ts
@@ -329,7 +329,7 @@ const stageChanges = async (changeCount: number): Promise<void> => {
 		await commands.stageFiles();
 		
 		const fileWord: string = changeCount === 1 ? CHANGE_MESSAGES.SINGLE_FILE : CHANGE_MESSAGES.MULTIPLE_FILES;
-		const stageMessage: string = `${changeCount} ${fileWord} committed`;
+		const stageMessage: string = `${changeCount} ${fileWord} successfully staged`;
 		
 		spinner.success({ text: white(stageMessage) });
 	} catch (error: any) {
