77b33504f1f8e1382ba8e3775dee5bf616f2829c
Josh Levy <joshua.a.levy@xcelenergy.com>
2025-11-13 11:15:28 -0700

    Finished refactor stage 3

--- bin/index.js
+++ bin/index.js
@@ -6,22 +6,36 @@ const { version } = require('../package.json');
 
 program
 	.description('Example: gitquick "I fixed a bug"')
-	.argument('[message]')
+	.argument('[message]', 'Commit message')
+	.option('-d, --debug', 'Enable debug mode with verbose output')
+	.option('--verbose', 'Enable verbose output (alias for --debug)')
 	.version(version, '-v, --version')
-	.action(async (message) => {
-		const processArgs = process.argv.slice(2);
-		if (processArgs.length > 1) {
-			message = processArgs.slice(0, processArgs.length).join(' ');
+	.action(async (message, options) => {
+		// Enable debug mode if flag is set
+		if (options.debug || options.verbose) {
+			runner.setDebugMode(true);
 		}
+
+		// Handle multi-word messages without quotes
+		const processArgs = process.argv.slice(2);
+		const flagArgs = processArgs.filter(arg => arg.startsWith('-'));
+		const messageArgs = processArgs.filter(arg => !arg.startsWith('-'));
+		
+		if (messageArgs.length > 1) {
+			message = messageArgs.join(' ');
+		}
+
 		try {
 			if (!message) message = await promptCommitMessage();
 			if (message) {
 				return await runner(message);
 			} else {
 				console.log('Unable to initiate commit process. Please try again.');
+				process.exit(1);
 			}
 		} catch (error) {
-			console.error(error);
+			console.error(error.message || error);
+			process.exit(1);
 		}
 	});
 
